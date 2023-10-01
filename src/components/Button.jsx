c30ebb6e9cd4eace75ea84a7d021ba38761c6f27
JsusMarcano <jesusjaviermarcanomora.2005@gmail.com>
2023-10-01 00:51:55 -0400

    fix: (style) change button font weight

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ export function Button(props) {
     <button
       {...props}
       className={cn(
-        "border border-gray-300 text-sm px-3 py-1 rounded-md bg-gradient-to-t from-gray-50 to-white",
+        "border border-gray-300 text-sm px-3 py-1 rounded-md bg-gradient-to-t from-gray-50 to-white font-medium",
         "dark:from-gray-900 dark:to-gray-800 dark:border-gray-700",
         "active:bg-gradient-to-b active:from-gray-100 active:to-white",
         props.className,
